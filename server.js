6dab5f567a69fb5f1b6568012422366d52df0725
kharl112 <kharl.dms112@gmail.com>
2021-12-01 22:14:03 -0600

    server.js completed waiting for bugs

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const { degrees, PDFDocument, rgb, StandardFonts } = require("pdf-lib");
+const { GoogleSpreadsheet } = require("google-spreadsheet");
 const fs = require("fs");
 const generatePDF = require("./src/methods/generatePDF");
+require("dotenv").config();
 
 app.use(express.json());
 app.use(express.static("public"));
@@ -13,13 +15,42 @@ app.get("/", (req, res) => {
 });
 
 app.post("/generate", async (req, res) => {
-  console.log(req.body);
   try {
-    const fileBuff = await generatePDF(req.body.ref_no);
+    const doc = new GoogleSpreadsheet(process.env.GOOGLE_SHEET_ID);
+
+    await doc.useServiceAccountAuth({
+      client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+      private_key: process.env.GOOGLE_SERVICE_ACCOUNT_PRIVATE_KEY,
+    });
+
+    await doc.loadInfo();
+    const sheet = doc.sheetsByIndex[0];
+    const rows = await sheet.getRows();
+    const fixed_rows = rows.map((node) => ({
+      name: node._rawData[3],
+      email: node._rawData[1],
+      id_no: node._rawData[2],
+      ref_no: node._rawData[5].replace(/\s/g, ""),
+    }));
+
+    const [participant] = fixed_rows.filter(
+      ({ id_no, ref_no }) =>
+        req.body.ref_no.replace(/\s/g, "") === ref_no &&
+        id_no === req.body.id_no
+    );
+
+    if (!participant)
+      return res
+        .status(404)
+        .send({
+          message:
+            "participant information was not found, please contact the organizing team if this is a mistake",
+        });
+
+    const fileBuff = await generatePDF(participant.name);
     res.send(fileBuff);
   } catch (error) {
-    console.log(error)
-    res.status(500).send({ message: "something wen't wrong" });
+    res.status(500).send({ message: "something went wrong" });
   }
 });
 
